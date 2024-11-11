4a42dbe16af507b3a42af1dbc0e930e82257a6bb
caio prezotto <152824103+caioprezottoo@users.noreply.github.com>
2024-11-11 11:39:39 -0300

    commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# ALURABOOKS ARRAY METHODS
+
+## About
+This project is about learning how to interact with arrays and manipulate objects. It was a fun project that taught me about Methods, such as "FIlter", "ForEach", "Map" and "Sort".
+
+## Tech
+- JavaScript
+
+## Author
+Caio Batista Prezotto
---
+++ app/metodoFilter.js
@@ -0,0 +1,23 @@
+// const btnFiltrarLivrosFront = document.getElementById('btnFiltrarLivrosFront')
+// btnFiltrarLivrosFront.addEventListener('click', filtrarLivros)
+
+const botoes = document.querySelectorAll('.btn')
+botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
+
+function filtrarLivros() {
+    const elementoBtn = document.getElementById(this.id)
+    const categoria = elementoBtn.value
+    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria)
+    exibirOsLivrosNaTela(livrosFiltrados)
+    if (categoria == 'disponivel') {
+        exibirValorTotalDosLivrosDisponiveisNaTela()
+    }
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela() {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+         <div class="livros__disponiveis">
+            <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+        </div>
+    `
+}
